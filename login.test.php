d2264a8564b9f763fc351060bb0417869ce3519e
Víctor Manuel Sánchez <vetorius@gmail.com>
2014-06-04 08:41:14 +0200

    cambiar a utf-8

--- login.test.php
+++ login.test.php
@@ -1,14 +1,22 @@
 <?php
 
 /*
- * Gestion de libros v 0.1
+ * Gestion de libros
  *
- * Copyright (c) 2005 Victor Manuel Sanchez
+ * Copyright (c) 2014 Victor manuel Sanchez
  *
- * archivo login.test.php
- *
- * validacion automatica para desarrollo sin servidor LDAP
- *
+   This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
  
 if (isset($_POST["usuario"])) {
@@ -32,7 +40,7 @@ if (fwrite($log_file, $contenido) === FALSE) {
 }
 fclose($log_file);
 
-// redirección a la pagina principal de la aplicacion
+// redirecciÃ³n a la pagina principal de la aplicacion
 
 header ("Location: aplicacion.php");
 ?>
